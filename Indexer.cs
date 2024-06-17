ca3a33589774a513c8b7bfa3966ee7dbf3f8da9c
Oleh Kozlovskiy <oleg.kozlovskiy.35p@gmail.com>
2024-06-17 20:34:31 +0300

    Fixind Method FindWordIndexes that incorrect count word position and negative influence on Add method

--- Indexer.cs
+++ Indexer.cs
@@ -20,31 +20,31 @@ public class Indexer : IIndexer
 		foreach(string word in words)
 		{
             List<int> wordsIndexesInText = new List<int>();
-            FindWordIndexes(words, word, ref wordsIndexesInText, 0);
+            FindWordIndexes(documentText, word, ref wordsIndexesInText, 0);
 			_documents[word] = new Dictionary<int, List<int>>() { { id, wordsIndexesInText } };
 		}
     }
 
-	public void FindWordIndexes(List<string> words, string wordToFind, ref List<int> indexes, int index)
+	public void FindWordIndexes(string documentText, string wordToFind, ref List<int> indexes, int index)
 	{
-		index = words.IndexOf(wordToFind, index);
-        if (index == -1 || index > words.Count)
+        index = documentText.IndexOf(wordToFind, index);
+        if (index == -1 || index == documentText.Length)
         {
             return;
         }
-        FindWordIndexes(words, wordToFind, ref indexes, index+1);
+        FindWordIndexes(documentText, wordToFind, ref indexes, index+1);
         indexes.Add(index);
     }
 
     public List<int> GetIds(string word)
 	{
-		return _documents.Where(x=>x.Key==word).SelectMany(x => x.Value.Select(y => y.Key)).ToList();
+		return _documents.Where(x => x.Key == word).SelectMany(x => x.Value.Select(y => y.Key)).ToList();
 	}
 
 	public List<int> GetPositions(int id, string word)
 	{
-		return _documents.Where(document => document.Key == word).SelectMany(y => y.Value.Where(y=>y.Key==id)
-			.SelectMany(y=>y.Value.Select(y=>y))).ToList();
+		return _documents.Where(document => document.Key == word).SelectMany(y => y.Value.Where( y=> y.Key == id)
+			.SelectMany(y => y.Value.Select(y => y))).ToList();
 	}
 
 	public void Remove(int id)
