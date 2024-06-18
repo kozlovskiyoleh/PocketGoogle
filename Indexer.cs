324c7f1905d1695d91af6221f08da7d5be12279c
Oleh Kozlovskiy <oleg.kozlovskiy.35p@gmail.com>
2024-06-18 20:55:36 +0300

    Find bugs in Add method

--- Indexer.cs
+++ Indexer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Avalonia.Markup.Xaml.Templates;
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
@@ -12,17 +13,27 @@ public class Indexer : IIndexer
 {
     private Dictionary<string, Dictionary<int, List<int>>> _documents = new();
 
+	//Bug here in that method
 	public void Add(int id, string documentText)
 	{
-		string pattern = @"\s|\?|\,|\.|\!|\:|\-|\n|\r";
+		string pattern = @"(\s)|(\?)|(\,)|(\.)|(\!)|(\:)|(\-)|(\n)|(\r)";
         Regex regex = new Regex(pattern);
 		List<string> words = regex.Split(documentText).ToList();
 		foreach(string word in words)
 		{
-            List<int> wordsIndexesInText = new List<int>();
-            FindWordIndexes(documentText, word, ref wordsIndexesInText, 0);
-			_documents[word] = new Dictionary<int, List<int>>() { { id, wordsIndexesInText } };
-		}
+			if (_documents.ContainsKey(word) && _documents[word].ContainsKey(id))
+			{
+				_documents[word][id].Add(documentText.IndexOf(word));
+            }
+			else if (_documents.ContainsKey(word))
+			{
+                _documents[word].Add(id, new List<int>(documentText.IndexOf(word)));
+            }
+			else
+			{
+                _documents.Add(word, new Dictionary<int, List<int>> { { id, new List<int>(documentText.IndexOf(word)) } });
+            }
+        }
     }
 
 	public void FindWordIndexes(string documentText, string wordToFind, ref List<int> indexes, int index)
@@ -41,14 +52,19 @@ public class Indexer : IIndexer
 		return _documents.Where(x => x.Key == word).SelectMany(x => x.Value.Select(y => y.Key)).ToList();
 	}
 
+	//Refactor this
 	public List<int> GetPositions(int id, string word)
 	{
-		return _documents.Where(document => document.Key == word).SelectMany(y => y.Value.Where( y=> y.Key == id)
-			.SelectMany(y => y.Value.Select(y => y))).ToList();
+		List<int> temp = _documents.Where(document => document.Key == word).SelectMany(y => y.Value.Where(y => y.Key == id)
+            .SelectMany(y => y.Value.Select(y => y))).ToList();
+		return temp;
 	}
 
 	public void Remove(int id)
 	{
-		//_documents.Remove(id);
+		foreach(var document in _documents.Select(x=>x.Value))
+		{
+			document.Remove(id);
+		}
 	}
 }
